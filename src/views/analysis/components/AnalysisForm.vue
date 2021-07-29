77006779fe5ed294c00bc0293fcdb81051467747
chenhui <gaischen@163.com>
2021-07-29 11:01:31 +0800

    analysis form

---
+++ src/views/analysis/components/AnalysisForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-form ref="postForm" :model="postForm" :rules="rules" class="form-container">
+
+  </el-form>
+</template>
+
+<script>
+const defaultForm = {
+  fileId: '',
+  startTime: undefined,
+  endTime: undefined
+}
+
+export default {
+  data: {
+    return: {
+      postForm: Object.assign({}, defaultForm)
+    }
+  }
+}
+</script>
---
+++ src/views/analysis/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="analysis-container">
+    <div class="analysis-text">name: {{ name }} token: {{ token }}
+      <analysis-form/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import AnalysisForm from './components/AnalysisForm'
+
+export default {
+  name: 'Analysis',
+  computed: {
+    ...mapGetters([
+      'name', 'token'
+    ])
+  },
+  components: {
+    AnalysisForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.analysis {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
